32afc0061ede3648e241a9ae9ca62a7573e8f526
Robert Vernam <rov959@g.harvard.edu>
2022-09-19 00:57:30 +0000

    controller-view setup

--- p1/index.php
+++ p1/index.php
@@ -1,14 +1,1 @@
-<!doctype html>
-<html lang='en'>
-
-<head>
-    <title>My DigitalOcean server</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-</head>
-
-<body>
-    <h1>Project 1!</h1>
-</body>
-
-</html>
+require "index-view.php";
