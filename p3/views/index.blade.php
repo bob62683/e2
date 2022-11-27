c62b566afcc9d171e13cc5eeb5d168fcc8a19627
Robert Vernam <rov959@g.harvard.edu>
2022-11-27 18:09:36 +0000

    E2P3 Initial Setup

---
+++ p3/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('templates/master')
+
+@section('title')
+{{ $welcome }}
+@endsection
+
+@section('content')
+
+<h2>Rock, Paper, Scissors, Lizard, Spock!</h2>
+<p><img src='images/RPSLS.webp' alt='Rock, Paper, Scissors, Lizard, Spock!'></p>
+<footer></footer>
+
+@endsection
